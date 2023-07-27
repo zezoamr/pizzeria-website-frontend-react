ab5f5bb4ea2bac42faee92928c4779ca188ab088
ziad amr shalaby <zezoamr@yandex.com>
2023-07-27 12:42:46 +0300

    Home page + Footer + navbar toggling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../assets/pizzaLogo.png'
 import { Link } from 'react-router-dom';
 import "../styles/Navbar.css"
 import ReorderIcon from '@mui/icons-material/Reorder'; 
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(currentState => {return !currentState})
+  }
+
   return (
     <div className='navbar'>
-        <div className='leftSide'>
-            <img src={logo} alt='image here'/>
+        <div className='leftSide' id={openLinks ? "open" : "close"}>
+            <img src={logo} alt='not found'/>
             <div className="hiddenLinks">
               <Link to="/"> Home </Link>
               <Link to="/menu"> Menu </Link>
@@ -21,7 +28,7 @@ function Navbar() {
             <Link to='/menu'> Menu </Link>
             <Link to='/about'> About </Link>
             <Link to='/contact'> Contact </Link>
-            <button>
+            <button onClick={toggleNavbar}>
               <ReorderIcon/>
             </button>
           </div> 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/pizza.jpeg'
+import "../styles/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'style={{ backgroundImage: `url( ${BannerImage} )` }}>
+        <div className='headerContainer'>
+            <h1>Da Pizzeria</h1>
+            <p>PIZZA THAT FITS EVERY TASTE</p>
+            <Link to='/menu'>  
+                <button > Order Now </button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
